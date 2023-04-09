e63569f0068c6036855e18b8e00530acc005bc25
Juilliard <Juilliardwu0405@gmail.com>
2023-04-09 14:44:49 -0400

    Add U.S system reporter that inlude the construtor, setter and getter

---
+++ Module11Problem1/src/main/java/com/juilliardwu/module11problem1/Main.java
@@ -0,0 +1,18 @@
+package com.juilliardwu.module11problem1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author juilliardwu
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Module11Problem1/src/main/java/com/juilliardwu/module11problem1/USsystemReporter.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.juilliardwu.module11problem1;
+
+/**
+ *
+ * @author juilliardwu
+ */
+public class USsystemReporter {
+    public int volts;
+
+    public USsystemReporter(int volts) {
+        this.volts = volts;
+    }
+
+    public int getVolts() {
+        return volts;
+    }
+
+    public void setVolts(int volts) {
+        this.volts = volts;
+    }
+    
+    
+}
